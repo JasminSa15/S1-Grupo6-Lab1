cd10cf4ad98607539966965186bfa9bf18e03064
nik-0x <brittanynicolfunesmartinez@gmail.com>
2026-08-17 00:00:25 -0600

    Add output for Brittany Nicol Funes Martinez

--- Program.cs
+++ Program.cs
@@ -1,1 +1,2 @@
 Console.WriteLine("Nombre: Jasmin Sarai Salgado Funes. -Aporte: Comparación entre desarrollo tradicional y desarrollo ágil.")
+  Console.WriteLine("Nombre: Brittany Nicol Funes Martinez. -Aporte: Conceptos fundamentales de la Ingeniería de Software.");
